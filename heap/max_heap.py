836783773fd1bce500c1a7030ddfb74317e3892c
Dillon McElhinney <dillon.mce@gmail.com>
2019-05-07 14:31:10 -0600

    Finish max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -2,20 +2,61 @@ class Heap:
   def __init__(self):
     self.storage = []
 
+  def __str__(self):
+    return self.storage.__str__()
+
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage)-1)
 
   def delete(self):
-    pass
+    value = self.get_max()
+    if len(self.storage) > 1:
+      new_value = self.storage.pop()
+      self.storage[0] = new_value
+      self._sift_down(0)
+    else:
+      self.storage.pop()
+
+    return value
+    
 
   def get_max(self):
-    pass
+    try:
+      return self.storage[0]
+    except:
+      return None
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    if index == 0:
+      return
+    parent_index = ((index - 1) // 2)
+    parent = self.storage[parent_index]
+    if self.storage[index] > parent:
+      self._swap(index, parent_index)
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
-    pass
+    child_1_index = 2*index + 1
+    child_2_index = 2*index + 2
+    if child_1_index >= len(self.storage):
+      return
+    child_1 = -1
+    child_2 = -1
+    try:
+      child_1 = self.storage[child_1_index]
+      child_2 = self.storage[child_2_index]
+    except:
+      pass
+    if child_1 > self.storage[index] and child_1 > child_2:
+      self._swap(index, child_1_index)
+      self._sift_down(child_1_index)
+    elif child_2 > self.storage[index]:
+      self._swap(index, child_2_index)
+      self._sift_down(child_2_index)
+
+  def _swap(self, a, b):
+    (self.storage[a], self.storage[b]) = (self.storage[b], self.storage[a])
